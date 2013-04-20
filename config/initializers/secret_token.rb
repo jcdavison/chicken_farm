d31eec10b007e144a8d64d8defa3d763317a614c
steveroger <steveroger@berkeley.edu>
2013-04-20 12:08:34 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChickenFarm::Application.config.secret_token = 'e688e35181eb0b5f615e92f38341f40b41e081148fb4aa6db2064f964e81715bb0bad3d92fda030162ffe111ec40794c7d70ba5febab6cf2789c953cfa5e6446'
