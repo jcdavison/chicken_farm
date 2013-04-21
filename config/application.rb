c350d584e3fc9c7b7d241d8d420de4999d951c87
Jeremia Kimelman <jbkimelman@gmail.com>
2013-04-20 18:08:36 -0700

    mixtakes and uri

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'Net::HTTP'
+require 'net/http'
+require 'uri'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
