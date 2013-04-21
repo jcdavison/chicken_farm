4ac560d929058e9e2ccc6c57d3dafa70e9eb3930
steveroger <steveroger@berkeley.edu>
2013-04-21 12:53:00 -0700

    chart

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
   def index
+
+  end
   
+  def test
+    # Array of the last 10 (value, datetime) for each sensor
+    @inputs1 = Sensor.find(1).inputs.take(5)
+    #@inputs2 = Sensor.find(2).inputs.take(5)
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ ChickenFarm::Application.routes.draw do
   match '/api/get_all_data', :to => 'api#get_all_data'
   match '/api/status', :to => 'api#status'
   root :to => 'main#index'
+  
+  match '/test', :to => 'main#test'
+  
 end
