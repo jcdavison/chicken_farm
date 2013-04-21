d26e83fc7cb90e380d0e61d5330698f4a520881c
steveroger <steveroger@berkeley.edu>
2013-04-21 13:38:29 -0700

    chart stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,12 @@ class MainController < ApplicationController
   def test
     # Array of the last 10 (value, datetime) for each sensor
     @inputs1 = Sensor.find(1).inputs.take(5)
+    @data1 = [ [Date.today.to_time.to_i , 1], [Date.today.to_time.to_i + 2, 5] ]
+    if @inputs1 != []
+      @inputs.each do |input|
+        @data1.push [input.created_at.to_time.to_i, input.value]
+      end
+    end
     #@inputs2 = Sensor.find(2).inputs.take(5)
     
   end
